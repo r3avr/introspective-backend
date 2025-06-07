881b7ed83085703cb8093131def2a3fd320f0c11
Marco Pruneddu <marcopruneddu@Mac.lan>
2025-06-07 14:13:24 -0400

    fix: remove redundant routes.py to resolve import conflict

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -7,18 +8,18 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'supersecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/site.db'  # or adjust as needed
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    from .models import User
+    from app.models import User
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .routes.main import main as main_blueprint
+    from app.routes.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
